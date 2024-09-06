510f81e37cd7207d88ef5073452f9eb534bc32b0
khanhphamxd <126229587+khanhphamxd@users.noreply.github.com>
2024-09-06 21:34:21 +1000

    Update CustomerDAO.java instruction

--- model/CustomerDAO.java
+++ model/CustomerDAO.java
@@ -7,7 +7,7 @@ public class CustomerDAO {
 
     // Database connection can be created here (add customers to database, retrieve all customers)
     // It should also have a "permission" tag to separate customers from staffs
-    // Login validation & check for unique username
+    // Include a query to search for a customer based on the username
     // NOTE: For any customer construction call for data extraction, please include the ID in the parameter of the constructor so that it won't mess with my ID tracker
 
 }
