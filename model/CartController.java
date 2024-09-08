ebb08dcb5a2194b21775bf5deb7ddc945c4ddb2c
Yao Zhang <126229558+wwdaas@users.noreply.github.com>
2024-09-08 15:49:30 +1000

    Create CartController.java

---
+++ model/CartController.java
@@ -0,0 +1,25 @@
+public class CartController {
+
+    private Cart cart;
+
+    // Constructor to create a new CartController with a new Cart
+    public CartController() {
+        this.cart = new Cart();
+    }
+
+    // Add product to cart
+    public void addToCart(Product product, int amount) {
+        cart.addToCart(product, amount);
+    }
+
+    // Remove product from cart
+    public void removeFromCart(Product product, int amount) {
+        cart.removeFromCart(product, amount);
+    }
+
+    // View cart
+    public void viewCart() {
+        cart.viewCart();
+    }
+}
+
